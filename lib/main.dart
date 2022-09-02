0fcbb0cc437e2071965f43061acf321e0b0c33ca
Aniket Jain <jain30aniket@gmail.com>
2022-09-02 13:39:47 +0530

    Sign up to login and vice-versa navigation

--- lib/main.dart
+++ lib/main.dart
@@ -81,94 +81,96 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     TextEditingController _emailController = TextEditingController();
     TextEditingController _passwordController = TextEditingController();
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Help Me Recall",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 44.0,
-              fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Help Me Recall",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          const Text(
-            "Login",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
+            const SizedBox(
+              height: 12.0,
             ),
-          ),
-          const SizedBox(
-            height: 44.0,
-          ),
-          TextField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              hintText: "User Email",
-              prefixIcon: Icon(Icons.mail, color: Colors.black),
+            const Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 26.0,
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(
-              hintText: "User Password",
-              prefixIcon: Icon(Icons.lock, color: Colors.black),
+            const SizedBox(
+              height: 44.0,
             ),
-          ),
-          const SizedBox(
-            height: 12.0,
-          ),
-          const Text(
-            "Forgot password ?",
-            style: TextStyle(color: Colors.blue),
-          ),
-          const SizedBox(
-            height: 66.0,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: RawMaterialButton(
-              fillColor: const Color(0xFF0069FE),
-              elevation: 0.0,
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0)),
-              onPressed: () async {
-                User? user = await loginUsingEmailPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                    context: context);
-                print(user);
-                if (user != null) {
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => ProfileScreen()));
-                }
-              },
-              child: const Text("Login",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                  )),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "User Email",
+                prefixIcon: Icon(Icons.mail, color: Colors.black),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 26.0,
-          ),
-          signUpOption()
-        ],
+            const SizedBox(
+              height: 26.0,
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "User Password",
+                prefixIcon: Icon(Icons.lock, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            const Text(
+              "Forgot password ?",
+              style: TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(
+              height: 66.0,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: RawMaterialButton(
+                fillColor: const Color(0xFF0069FE),
+                elevation: 0.0,
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                onPressed: () async {
+                  User? user = await loginUsingEmailPassword(
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                      context: context);
+                  print(user);
+                  if (user != null) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => ProfileScreen()));
+                  }
+                },
+                child: const Text("Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 26.0,
+            ),
+            signUpOption()
+          ],
+        ),
       ),
     );
   }
--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,3 +1,5 @@
+import 'package:counter/main.dart';
+import 'package:counter/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -10,10 +12,108 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
     return Scaffold(
-      body: Center(
-        child: Text("Welcome to your sign up page"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Help Me Recall",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            const Text(
+              "Sign Up",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 44.0,
+            ),
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "User Email",
+                prefixIcon: Icon(Icons.mail, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 26.0,
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "User Password",
+                prefixIcon: Icon(Icons.lock, color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            const SizedBox(
+              height: 56.0,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: RawMaterialButton(
+                fillColor: const Color(0xFF0069FE),
+                elevation: 0.0,
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => ProfileScreen()));
+                },
+                child: const Text("Sign Up",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 26.0,
+            ),
+            logInOption()
+          ],
+        ),
       ),
+    );
+  }
+
+  Row logInOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Already have an account?",
+            style: TextStyle(color: Colors.black)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LoginScreen()));
+          },
+          child: const Text(
+            " Log In",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
     );
   }
 }
