5cd629ab8f72e7b13d2f1a8bccf90b4bb2aeb211
Aniket Jain <jain30aniket@gmail.com>
2022-10-12 21:00:25 +0530

    upload documents module integrated

--- lib/upload_screen.dart
+++ lib/upload_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:dio/dio.dart';
 
 class UploadScreen extends StatefulWidget {
   const UploadScreen({Key? key}) : super(key: key);
@@ -8,11 +12,66 @@ class UploadScreen extends StatefulWidget {
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  int _counter = 0;
+  XFile? _image;
+  final GlobalKey<ScaffoldState> _scaffoldstate =
+      new GlobalKey<ScaffoldState>();
+
+  Future getImage() async {
+    var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    _uploadFile(image);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _uploadFile(filePath) async {
+    String fileName = basename(filePath.path);
+    print("File base name: $fileName");
+
+    try {
+      FormData formData = FormData.fromMap({
+        "file": await MultipartFile.fromFile(filePath.path, filename: fileName)
+      });
+
+      Response response =
+          await Dio().post("http://192.168.0.101/saveFile.php", data: formData);
+      print("File upload response: ${response.data}");
+
+      _showSnakBarMsg(response.data['message']);
+    } catch (e) {
+      print("Exception Caught: $e");
+    }
+  }
+
+  void _showSnakBarMsg(String msg) {
+    ScaffoldMessenger.of(this.context)
+        .showSnackBar(SnackBar(content: Text(msg)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldstate,
+      appBar: AppBar(
+        title: Text('Upload Documents'),
+      ),
       body: Center(
-        child: Text("UPLOAD SCREEN"),
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(File(_image!.path)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
